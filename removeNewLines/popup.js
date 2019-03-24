1ae9078bdec3dc4b77315409e730836cb9a8a1ba
m_tantan <yontymanor@gmail.com>
2019-03-24 10:26:46 +0200

    Made popup window prettier

--- removeNewLines/popup.js
+++ removeNewLines/popup.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
     console.log(document.body);
     let elem = document.createElement("P");
     elem.innerHTML = textToEdit;
+    elem.classList.add("text_class");
     console.log(elem);
     document.body.appendChild(elem);
   });
