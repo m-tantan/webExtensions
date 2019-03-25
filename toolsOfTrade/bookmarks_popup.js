0d51301ae699d128991b97bb6d38525042789364
m_tantan <yontymanor@gmail.com>
2019-03-26 00:02:10 +0200

    Managed to make a bookmark within a folder :)

--- toolsOfTrade/bookmarks_popup.js
+++ toolsOfTrade/bookmarks_popup.js
@@ -10,16 +10,25 @@ document.addEventListener('DOMContentLoaded', function(event) {
     //                           'title': 'MyFolder/Extensions doc',
     //                             'url': 'http://code.google.com/chrome/extensions'
     //                           });  
-    var res = chrome.bookmarks.create({ 
-      'parentId': "1",
-      'title': 'Extension bookmarks'}
-    );
+    // var res = chrome.bookmarks.create({ 
+    //   'parentId': "1",
+    //   'title': 'Extension bookmarks'}
+    // );
 
     chrome.bookmarks.create({
-      'title': "Extension bookmarks\\InsideBookmark",
-      'url': 'https://www.ynet.co.il/home/0,7340,L-8,00.html',
+      'title': "Folder",
       'index': 0,
-      'parentId': "shalom"
-    });
-  
+      'parentId': "1"
+    }, createUrlWithin);
 })
+
+function createUrlWithin(newBookmark){
+  console.log("added bookmark with name: " + newBookmark.title);
+  console.log("In general, the bookmark object is this: ", newBookmark);
+
+  chrome.bookmarks.create({
+    'title': newBookmark.title + "link",
+    'url': "http://code.google.com/chrome/extensions", 
+    'parentId': newBookmark["id"]
+  });
+}
