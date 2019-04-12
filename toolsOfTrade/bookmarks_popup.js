a094cfbaef0d232c71b8d9daf238ac8a4da7179e
Yonatan Manor <t-yomano@microsoft.com>
2019-04-12 11:13:50 +0300

    Add img to li, clear works

--- toolsOfTrade/bookmarks_popup.js
+++ toolsOfTrade/bookmarks_popup.js
@@ -8,18 +8,19 @@ const BOOKMARKS_BAR = 1;
 const OTHER_BOOKMARKS_FOLDER = 2;
 const MOBILE_BOOKMARKS = 3;
 
-const INITIALIZE = 0;
 const GET_CURRENT_BOOKMARK_FOLDERS = 1;
 const OPEN_AS_TABS = 2;
 const SAVE_URLS_TO_BOOKMARK_FOLDER = 3;
 
 // Pre-script preparation 
-var mode = INITIALIZE;
+var mode = GET_CURRENT_BOOKMARK_FOLDERS;
 var parentFolderId = BOOKMARKS_BAR;
+var loadedFolders = false;
 const generalListOfBookmarks = [BOOKMARKS_BAR, OTHER_BOOKMARKS_FOLDER, MOBILE_BOOKMARKS]
 let bgPage = chrome.extension.getBackgroundPage();  // Gets the background script code, can access window variables from here
 var tabsToOpenList = [];
 var linksToSaveList = [];
+var foldersDict = {};
 
 // Waits for the popup to load. Code goes in here
 document.addEventListener('DOMContentLoaded', function (event) {
@@ -35,13 +36,15 @@ document.addEventListener('DOMContentLoaded', function (event) {
   var radioButtons = document.getElementsByClassName("form-check-input");
   var newFolderInputText = document.getElementById("folderNameId");
   var newFolderNameInput = document.getElementById("folderNameInput");
+  var availableFoldersDiv = document.getElementById("mydiv");
+  
 
   // 'Define' radio buttons 
-  const shareFoldersRadio = radioButtons[0];
-  const openAsTabsRadio = radioButtons[1];
-  const importNewFolderFromUrlRadio = radioButtons[2];
+  var shareFoldersRadio = radioButtons[0];
+  var openAsTabsRadio = radioButtons[1];
+  var importNewFolderFromUrlRadio = radioButtons[2];
 
-  // Setu radio buttons' onclick
+  // Setup radio buttons' onclick
   shareFoldersRadio.onclick = function (ev) {
     mode = GET_CURRENT_BOOKMARK_FOLDERS;
     console.log("Will get all bookmarks");
@@ -49,9 +52,11 @@ document.addEventListener('DOMContentLoaded', function (event) {
     urlTextGuidance.classList.add("hideElem");
     newFolderInputText.classList.add("hideElem");
     newFolderNameInput.classList.add("hideElem");
+    clearBtn.classList.add("hideElem");
 
-    document.body.style.height = '140px';
+    document.body.style.height = '145px';
   };
+
   openAsTabsRadio.onclick = function (ev) {
     mode = OPEN_AS_TABS;
     console.log("Copied all active tabs");
@@ -59,9 +64,11 @@ document.addEventListener('DOMContentLoaded', function (event) {
     urlTextGuidance.classList.remove("hideElem");
     newFolderInputText.classList.add("hideElem");
     newFolderNameInput.classList.add("hideElem");
-    
+    clearBtn.classList.remove("hideElem");
+
     document.body.style.height = '450px';
   };
+
   importNewFolderFromUrlRadio.onclick = function (ev) {
     mode = SAVE_URLS_TO_BOOKMARK_FOLDER;
     console.log("Creating a new bookmark folder from given links");
@@ -69,39 +76,46 @@ document.addEventListener('DOMContentLoaded', function (event) {
     urlTextGuidance.classList.remove("hideElem");
     newFolderInputText.classList.remove("hideElem");
     newFolderNameInput.classList.remove("hideElem");
-    document.body.style.height = '460px';
+    clearBtn.classList.remove("hideElem");
+    
+    document.body.style.height = '475px';
   };
 
   // Set up buttons' onclick here
   clearBtn.onclick = function (ev) {
     console.log("Deleting textarea content");
+    urlTextArea.value = "";
   };
 
-  bookmarkBarRadio.onclick = function(ev) {
+  bookmarkBarRadio.onclick = function (ev) {
     parentFolderId = BOOKMARKS_BAR;
     console.log(`Select BookmarkBar`);
   };
 
-  otherBookmarksRadio.onclick = function(ev) {
+  otherBookmarksRadio.onclick = function (ev) {
     parentFolderId = OTHER_BOOKMARKS_FOLDER;
     console.log(`Select Other bookmark folder`);
   };
 
   proceedBtn.onclick = function (ev) {
     console.log("Proceed btn clicked");
-
+    /* --------------------- SHARING FROM LIST OF FOLDERS --------------------------- */
     // If user wishes to open several links together
-    if (mode == GET_CURRENT_BOOKMARK_FOLDERS || mode == INITIALIZE){
+    if (mode == GET_CURRENT_BOOKMARK_FOLDERS) {
+      console.log("Supposed to display all bookmarks on load");
       generalListOfBookmarks.forEach(cat => {
-        var catAsStr = cat.toString();
-        chrome.bookmarks.get([catAsStr], onFetchBookmarkSuccess);
-      });
-
+        var catagoryAsStr = cat.toString();
+        chrome.bookmarks.get([catagoryAsStr], onFetchBookmarkSuccess);
+      });   
     }
-    else if (mode == OPEN_AS_TABS) { 
+    
+    
+    /* ---------------------------- OPEN AS TABS ------------------------------------ */
+    
+    else if (mode == OPEN_AS_TABS) {
       var unparsedUrlsToOpen = urlTextArea.value;
       var parsedUrlsToOpen = unparsedUrlsToOpen.split("\n");
-      if (unparsedUrlsToOpen.length > 0){
+      if (unparsedUrlsToOpen.length > 0) {
         for (i = 0; i < parsedUrlsToOpen.length; i++) {
           website = parsedUrlsToOpen[i];
           console.log("Opening new tab with: ", website);
@@ -109,45 +123,48 @@ document.addEventListener('DOMContentLoaded', function (event) {
         }
       }
     }
-    else if (mode == SAVE_URLS_TO_BOOKMARK_FOLDER){ // If Create a folder was selected
+    /* ---------------------------- SAVE TO FOLDER ----------------------------------- */
+    
+    else if (mode == SAVE_URLS_TO_BOOKMARK_FOLDER) { // If Create a folder was selected
       var unparsedUrlsToOpen = urlTextArea.value;
-      if (unparsedUrlsToOpen.length > 0){
+      if (unparsedUrlsToOpen.length > 0) {
         var parsedUrlsToOpen = unparsedUrlsToOpen.split("\n");
-
+        
         for (i = 0; i < parsedUrlsToOpen.length; i++) {
           website = parsedUrlsToOpen[i];
           console.log("Saving new bookmark: ", website);
         }
+        
         linksToSaveList = parsedUrlsToOpen;
         chrome.bookmarks.create({
           'title': newFolderNameInput.value.toString(),
           'index': 0,
           'parentId': parentFolderId.toString()
-        }, createUrlsWithin);
-        
+        }, createUrlsWithinNewFolder);        
       }
     }
-  };
-
-  // Used to fetch a folder / single url
-  
-  // // Used to get the children within a folder with a known Id
-  if (activated) {
-    var fetchPromiseChildren = chrome.bookmarks.getChildren("255", onFetchBookmarkSuccess);
-    var fetchPromiseFolder = chrome.bookmarks.get(["255"], onFetchBookmarkSuccess);
-    chrome.bookmarks.create({
-      'title': "Folder",
-      'index': 0,
-      'parentId': "0"
-    }, createUrlsWithin);
+        
+    // Used to fetch a folder / single url
+    
+    // // Used to get the children within a folder with a known Id
+    if (activated) {
+      var fetchPromiseChildren = chrome.bookmarks.getChildren("255", onFetchBookmarkSuccess);
+      var fetchPromiseFolder = chrome.bookmarks.get(["255"], onFetchBookmarkSuccess);
+      chrome.bookmarks.create({
+        'title': "Folder",
+        'index': 0,
+        'parentId': "0"
+      }, createUrlsWithinNewFolder);
+    }
   }
+  loadAllBookmarks();
 })
 
-function createUrlsWithin(newBookmarkFolder, listOfLinks) {
+function createUrlsWithinNewFolder(newBookmarkFolder, listOfLinks) {
   console.log("added bookmark folder with name: " + newBookmarkFolder.title);
   console.log("In general, the bookmark object is this: ", newBookmarkFolder);
   console.log(`listOfLinks: ${listOfLinks}`)
-  linksToSaveList.forEach( (link) => {
+  linksToSaveList.forEach((link) => {
     var getNameRegex = /^https?:\/\/(www)?\.?([\w]*).*$/gm;
     var matches = getNameRegex.exec(link);
     var urlName = matches[2];
@@ -160,38 +177,51 @@ function createUrlsWithin(newBookmarkFolder, listOfLinks) {
   });
 }
 
-var listOfBookmarks = [];
-
-function getAllBookmarks() {
-  generalListOfBookmarks.forEach(cat => {
-
-  });
-}
-
 function onFetchBookmarkSuccess(bookmarks) {
   bookmarks.forEach(element => {
     console.log(element);
-    console.log(element.id);
     chrome.bookmarks.getChildren(element.id, onGetChildrenSuccess)
   });
 }
 
-function onGetChildrenSuccess(children){
-  // var listOfFolderNames = [];
-  // var listOfFolderIds = [];
-  var foldersDict = {};
-  children.forEach((child)=> {
-    if (child.url == null){
+function onGetChildrenSuccess(children) {
+  children.forEach((child) => {
+    if (child.url == null) {
       console.log(`Found folder: ${child.title}`)
-      // listOfFolderNames.add(child.title);
-      // listOfFolderIds.add(child.id);
-      foldersDict[child.title] = child.id;
+      foldersDict[child.title] = child.title;
     }
   })
-  
-  return foldersDict;
 }
 
 function onFetchBookmarkFail(err) {
   console.log(`Err: ${err}`);
+}
+
+function loadAllBookmarks(elemToAddTo){
+  let j = document.getElementById("mydiv");
+  console.log(`The elem is: ${elemToAddTo}`);
+  console.log(`The elem is: ${j}`);
+  
+  if (!loadedFolders){
+    // loadedFolders = true;
+    let availableFoldersDiv = document.getElementById("mydiv");
+
+    let unorderedList = document.createElement("ul");
+    unorderedList.id = "ulId";
+    availableFoldersDiv.appendChild(unorderedList);
+    loadFoldersIntoUl();
+  }
+}
+
+
+
+function loadFoldersIntoUl(htmlElem){
+  let ul = document.getElementById("ulId");
+
+  for (var key in foldersDict){
+    var elem = document.createElement("li");
+    elem.innerHTML = foldersDict[key];
+    elem.classList.add("folder");
+    ul.appendChild(elem);
+  };
 }
