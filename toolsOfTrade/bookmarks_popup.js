6c723501edeaa30326422f4578c91dd4cff2a321
Yonatan Manor <t-yomano@microsoft.com>
2019-04-12 11:34:02 +0300

    Can copy to clipboard using copyTextToClipboard

--- toolsOfTrade/bookmarks_popup.js
+++ toolsOfTrade/bookmarks_popup.js
@@ -142,6 +142,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
           'parentId': parentFolderId.toString()
         }, createUrlsWithinNewFolder);        
       }
+      copyTextToClipboard(urlTextArea.value);
     }
         
     // Used to fetch a folder / single url
@@ -213,8 +214,6 @@ function loadAllBookmarks(elemToAddTo){
   }
 }
 
-
-
 function loadFoldersIntoUl(htmlElem){
   let ul = document.getElementById("ulId");
 
@@ -224,4 +223,30 @@ function loadFoldersIntoUl(htmlElem){
     elem.classList.add("folder");
     ul.appendChild(elem);
   };
+}
+
+function copyTextToClipboard(text) {
+  //Create a textbox field where we can insert text to. 
+  var copyFrom = document.createElement("textarea");
+
+  //Set the text content to be the text you wished to copy.
+  copyFrom.textContent = text;
+
+  //Append the textbox field into the body as a child. 
+  //"execCommand()" only works when there exists selected text, and the text is inside 
+  //document.body (meaning the text is part of a valid rendered HTML element).
+  document.body.appendChild(copyFrom);
+
+  //Select all the text!
+  copyFrom.select();
+
+  //Execute command
+  document.execCommand('copy');
+
+  //(Optional) De-select the text using blur(). 
+  copyFrom.blur();
+
+  //Remove the textbox field from the document.body, so no other JavaScript nor 
+  //other elements can get access to this.
+  document.body.removeChild(copyFrom);
 }
